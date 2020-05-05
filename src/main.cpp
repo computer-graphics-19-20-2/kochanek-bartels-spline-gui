19196fbcd28270b36acaf970029a3ff9625d0727
Attila Bagossy <bagossyattila@outlook.com>
2020-05-05 15:07:41 +0200

    Added the Kochanek-Bartels starter code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,128 @@
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include "glad/glad.h"
+
+#include "bevgrafmath2017.h"
+
+
+struct color_t {
+	uint8_t r, g, b, a = 255;
+};
+
+void glColorCl(const color_t &cl) {
+	glColor4ub(cl.r, cl.g, cl.b, cl.a);
+}
+
+void glClearColorCl(const color_t &cl) {
+	glClearColor((float)cl.r / 255.f, (float)cl.g / 255.f, (float)cl.b / 255.f, (float)cl.a / 255.f);
+}
+
+const color_t BACKGROUND_COLOR = { 40, 40, 40 };
+const color_t CONTROL_POINT_COLOR = { 255, 255, 255 };
+const color_t CONTROL_POLYGON_COLOR = { 255, 255, 255 };
+const color_t CURVE_COLOR = { 255, 171, 64 };
+
+const float CURVE_WIDTH = 2.5f;
+const float CONTROL_POLYGON_WIDTH = 1.5f;
+
+const float CLICK_THRESHOLD = 100.0f;
+
+const float EVALUATION_PARAMETER_DELTA = 0.05f;
+
+std::vector<vec2> controlPoints;
+vec2 *draggedControlPoint = nullptr;
+
+float tension = 0.0f;
+float bias = 0.0f;
+float continuity = 0.0f;
+
+mat4 calculateCoefficientMatrix(const float tension, const float bias, const float continuity);
+void drawCurve(const mat4 &coefficientMatrix, const std::vector<vec2> &controlPoints);
+void drawSegment(const size_t segmentIndex, const mat4 &coefficientMatrix, const std::vector<vec2> &controlPoints);
+void drawControlPolygon(const std::vector<vec2> &controlPoints);
+void drawControlPoints(const std::vector<vec2> &controlPoints);
+
+vec2 *getClickedPoint(const vec2 &cursorPosition, std::vector<vec2> &controlPoints);
+
+
 int main(int argc, char **argv) {
     return 0;
+}
+
+mat4 calculateCoefficientMatrix(const float tension, const float bias, const float continuity) {
+	const float s = 0.5f * (1.0f - tension);
+	const float q1 = s * (1.0f + bias) * (1.0f - continuity);
+	const float q2 = s * (1.0f - bias) * (1.0f + continuity);
+	const float q3 = s * (1.0f + bias) * (1.0f + continuity);
+	const float q4 = s * (1.0f - bias) * (1.0f - continuity);
+
+	return {
+		{ -q1, 2.0f * q1, -q1, 0 },
+		{ q1 - q2 - q3 + 2.0f, q3 - (2.0f * q1) + (2.0f * q2) - 3.0f, q1 - q2, 1.0f },
+		{ q2 + q3 - q4 - 2.0f, q4 - q3 - (2.0f * q2) + 3.0f, q2, 0 },
+		{ q4, -q4, 0, 0}
+	};
+}
+
+void drawCurve(const mat4 &coefficientMatrix, const std::vector<vec2> &controlPoints) {
+	const size_t segmentCount = controlPoints.size() - 3;
+
+	glLineWidth(CURVE_WIDTH);
+	glColorCl(CURVE_COLOR);
+	glBegin(GL_LINE_STRIP);
+	for (size_t segmentIndex = 0; segmentIndex < segmentCount; ++segmentIndex) {
+		drawSegment(segmentIndex, coefficientMatrix, controlPoints);
+	}
+	glEnd();
+}
+
+void drawSegment(const size_t segmentIndex, const mat4 &coefficientMatrix, const std::vector<vec2> &controlPoints) {
+	const mat24 geometry = {
+		controlPoints[segmentIndex + 0],
+		controlPoints[segmentIndex + 1],
+		controlPoints[segmentIndex + 2],
+		controlPoints[segmentIndex + 3]
+	};
+
+	const mat24 gm = geometry * coefficientMatrix;
+
+	for (float t = 0.0f; t <= 1.0f + EVALUATION_PARAMETER_DELTA; t += EVALUATION_PARAMETER_DELTA) {
+		const vec4 parameterVector = { t * t * t, t * t, t, 1.0f };
+
+		const vec2 curvePoint = gm * parameterVector;
+
+		glVertex2f(curvePoint.x, curvePoint.y);
+	}
+}
+
+void drawControlPolygon(const std::vector<vec2> &controlPoints) {
+	glLineWidth(CONTROL_POLYGON_WIDTH);
+	glColorCl(CONTROL_POLYGON_COLOR);
+	glBegin(GL_LINE_STRIP);
+	for (const auto& point : controlPoints) {
+		glVertex2f(point.x, point.y);
+	}
+	glEnd();
+}
+
+void drawControlPoints(const std::vector<vec2> &controlPoints) {
+	glColorCl(CONTROL_POINT_COLOR);
+	glBegin(GL_POINTS);
+	for (const auto &point : controlPoints) {
+		glVertex2f(point.x, point.y);
+	}
+	glEnd();
+}
+
+vec2 *getClickedPoint(const vec2 &cursorPosition, std::vector<vec2> &controlPoints) {
+	const auto clickedIterator = std::find_if(controlPoints.begin(), controlPoints.end(), [&cursorPosition](const vec2 &point) {
+		return dist2(cursorPosition, point) <= CLICK_THRESHOLD;
+	});
+
+	return clickedIterator == controlPoints.end()
+		? nullptr
+		: &(*clickedIterator);
 }
